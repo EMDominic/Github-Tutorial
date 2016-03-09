7c20244322825a4755ec5ca5b36a0f9c1676af85
Emanuel-Muema-Dominic <emanuel@aims.ac.za>
2016-03-09 19:39:45 +0200

    how to correct an error in git rstudio

--- codes.R
+++ codes.R
@@ -1,2 +1,8 @@
 data("mtcars")
 summary(mtcars)
+
+# Go back to the file directory in the terminal
+# git remote add origin https://github.com/myname/test.git
+# git config remote.origin.url git@github.com:myname/test.git
+# git pull -u origin master
+# git push -u origin master
